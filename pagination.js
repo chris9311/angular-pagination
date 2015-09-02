c2d036205c115e76d77a3227b2a661fa0e6ff6d9
Chris Huang <chris.huang@cargosmart.com>
2015-09-02 17:26:41 +0800

    improve

--- pagination.js
+++ pagination.js
@@ -48,7 +48,7 @@ app.controller('appController',function($scope,$rootScope,$routeParams,$http){
         $http.get('list.json')
             .success(function(data){
 
-
+                console.log(data);
                 //this logic can use in the node + express
                 var current = (page - 1) * pagesize;//page start by 1 ,items start by 0
                 var items = data || [];
@@ -85,9 +85,19 @@ app.directive('pagination',function(){
         '<span aria-hidden="true">&laquo;</span> ' +
         '</a> ' +
         '</li> ' +
+        '<li> ' +
+        '<a ng-hide="sign == 1" href="" aria-label="Previous" ng-click="lastgroup()"> ' +
+        '<span aria-hidden="true">...</span> ' +
+        '</a> ' +
+        '</li> ' +
         '<li ng-repeat="num in totalPages" ng-class=\'{true: "active", false: "inactive"}[currentPage==num]\'> ' +
         '<a  href={{url}}/{{num}} >' +
         '{{num}} ' +
+        '</a> ' +
+        '</li> ' +
+        '<li> ' +
+        '<a ng-hide="sign >= pages-9" href="" aria-label="Next" ng-click="nextgroup()"> ' +
+        '<span aria-hidden="true">...</span> ' +
         '</a> ' +
         '</li> ' +
         '<li> ' +
@@ -110,28 +120,63 @@ app.directive('pagination',function(){
 
                 scope.pages = data.totalPages;
                 scope.totalPages = [];
-
-                for (var i = 0; i < scope.pages; i++) {
-                    scope.totalPages.push(i + 1);
-                }
-
-                scope.categoryid = data.categoryid;
+                //scope.sign=data.currentPage;
+                scope.sign = data.currentPage - data.currentPage%10 + 1 ;
                 scope.currentPage = data.currentPage;
                 scope.url = attrs.url;
-                if(attrs.urlAtt1){
+                if(scope.pages>10){
+                    for (var i = 0; i < 10; i++) {
+                        scope.totalPages.push(i + 1);
+                    }
+                }else{
+                    for (var i = 0; i < scope.pages; i++) {
+                        scope.totalPages.push(i + 1);
+                    }
+                }
+
+                scope.currentPage = data.currentPage;
+                scope.url = attrs.url;
+                if(attrs.urlAtt1Name){
                     scope.url =  scope.url +"/"+ data[attrs.urlAtt1Name];
                 }
-                if(attrs.urlAtt2){
+                if(attrs.urlAtt2Name){
                     scope.url =  scope.url +"/"+ data[attrs.urlAtt2Name] ;
+                }
+            });
+
+            scope.nextgroup = function(){
+                scope.sign = parseInt(scope.sign)+10
+            };
+            scope.lastgroup = function(){
+                if(parseInt(scope.sign)-10 >= 1){
+                    scope.sign = parseInt(scope.sign)-10;
+                }else{
+                    scope.sign = 1;
+                }
+            };
+
+            scope.$watch('sign',function(){
+                if(scope.sign){
+                    if(parseInt(scope.sign) >= parseInt(scope.pages)){
+                        scope.sign =scope.pages - scope.pages%10 + 1;
+                    }
+                    var sign2 = parseInt(scope.sign);
+                    var countpage = [];
+                    for(; sign2 < parseInt(scope.sign)+10 && sign2 <= parseInt(scope.pages);sign2++){
+                        countpage.push(sign2);
+                    }
+                    scope.totalPages = countpage;
                 }
             });
 
             scope.firstpage = function(){
                 window.location = scope.url +"/" + 1;
+                scope.sign = 1;
             };
 
             scope.lastpage = function(){
                 window.location = scope.url +"/" + scope.pages;
+                scope.sign =scope.pages - scope.pages%10 + 1;
             };
 
         }
